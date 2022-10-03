686dcf7e878cb303f0bf0141278daebe782429e6
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-10-03 14:49:12 -0600

    gh

---
+++ height.js
@@ -0,0 +1,20 @@
+//varis
+
+function checkHeight(height) {
+  if (height < 82) {
+    return "Too short for ride";
+  } else {
+    return "All Aboard!";
+  }
+}
+
+function analyzeNumber(number) {
+  if (number > 0) {
+    return "postive number";
+  } else if (number < 0) {
+    return "negative number";
+  } else {
+    return "zero";
+  }
+}
+
